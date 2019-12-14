b24d1e483f2515a78c287ce4954c381a05a04ea2
Jack Armstrong <mackycheese21@gmail.com>
2019-12-14 12:07:59 -0800

    nbt

--- src/nbt/nbt.h
+++ src/nbt/nbt.h
@@ -23,21 +23,21 @@
 
 namespace nbt {
 
-    std::shared_ptr<nbt> read_nbt(std::istream&in);
+    std::shared_ptr<nbt> read_nbt(std::istream&in)noexcept;
 
     template<typename T,typename V>
-    std::shared_ptr<nbt> make(const V& value){
+    std::shared_ptr<nbt> make(const V& value)noexcept{
         return std::shared_ptr<nbt>(new T(value));
     }
 
     template<typename T>
-    std::shared_ptr<T> cast(const std::shared_ptr<nbt>&value){
+    std::shared_ptr<T> cast(const std::shared_ptr<nbt>&value)noexcept{
         return std::dynamic_pointer_cast<T>(value);
     }
 
     extern const std::function<std::shared_ptr<nbt>(const short&value)> make_short;
     extern const std::function<std::shared_ptr<nbt>(const int&value)> make_int;
-    extern const std::function<std::shared_ptr<nbt>(const float&value)> make_float;
+    extern const std::function<std::shared_ptr<nbt>(const float&value)>make_float;
     extern const std::function<std::shared_ptr<nbt>(const long&value)> make_long;
     extern const std::function<std::shared_ptr<nbt>(const std::string&value)> make_string;
     extern const std::function<std::shared_ptr<nbt>(const std::map<std::string,std::shared_ptr<nbt>>&value)> make_compound;
