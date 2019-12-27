cf10509b46ebf7060a2d8922230876774f1d827d
Max Steinberg <maxssteinberg@gmail.com>
2019-12-27 12:22:27 -0800

    transferred nbt object over stream

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -5,6 +5,8 @@
 #include "game.h"
 #include <thread>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include "nbt/nbt.h"
 
 extern "C" const unsigned char TEXTURE_1_8_textures_0_png[];
 extern "C" const size_t TEXTURE_1_8_textures_0_png_len;
@@ -28,67 +30,69 @@ namespace client {
     }
 
     void game::initialize() {
-        world=std::make_shared<block::world>();
-        shader = std::make_shared<gl::shader>(SHADER_chunk_vert,SHADER_chunk_vert_len,SHADER_chunk_frag,SHADER_chunk_frag_len);
-        texture = std::make_shared<gl::texture>(TEXTURE_1_8_textures_0_png,TEXTURE_1_8_textures_0_png_len);
-        basic_shader=std::make_shared<gl::shader>(SHADER_wireframe_vert,SHADER_wireframe_vert_len,SHADER_wireframe_frag,SHADER_wireframe_frag_len);
+        world = std::make_shared<block::world>();
+        shader = std::make_shared<gl::shader>(SHADER_chunk_vert, SHADER_chunk_vert_len, SHADER_chunk_frag,
+                                              SHADER_chunk_frag_len);
+        texture = std::make_shared<gl::texture>(TEXTURE_1_8_textures_0_png, TEXTURE_1_8_textures_0_png_len);
+        basic_shader = std::make_shared<gl::shader>(SHADER_wireframe_vert, SHADER_wireframe_vert_len,
+                                                    SHADER_wireframe_frag, SHADER_wireframe_frag_len);
         gl::mesh_data cube_data;
-        cube_data.buffers.push_back({3,{
-            // 000 001 010 011 100 101 110 111
+        cube_data.buffers.push_back({3, {
+                // 000 001 010 011 100 101 110 111
 
-            0,0,0, 1,0,0,
-            0,0,0, 0,1,0,
-            0,0,0, 0,0,1,
+                0, 0, 0, 1, 0, 0,
+                0, 0, 0, 0, 1, 0,
+                0, 0, 0, 0, 0, 1,
 
-            0,0,1, 1,0,0,
-            0,0,1, 0,1,0,
-            0,0,1, 0,0,0,
+                0, 0, 1, 1, 0, 0,
+                0, 0, 1, 0, 1, 0,
+                0, 0, 1, 0, 0, 0,
 
-            0,1,0, 1,1,0,
-            0,1,0, 0,0,0,
-            0,1,0, 0,1,1,
+                0, 1, 0, 1, 1, 0,
+                0, 1, 0, 0, 0, 0,
+                0, 1, 0, 0, 1, 1,
 
-            0,1,1, 1,1,1,
-            0,1,1, 0,0,1,
-            0,1,1, 0,1,0,
+                0, 1, 1, 1, 1, 1,
+                0, 1, 1, 0, 0, 1,
+                0, 1, 1, 0, 1, 0,
 
-            1,0,0, 0,0,0,
-            1,0,0, 1,1,0,
-            1,0,0, 1,0,1,
+                1, 0, 0, 0, 0, 0,
+                1, 0, 0, 1, 1, 0,
+                1, 0, 0, 1, 0, 1,
 
-            1,1,0, 0,1,0,
-            1,1,0, 1,0,0,
-            1,1,0, 1,1,1,
+                1, 1, 0, 0, 1, 0,
+                1, 1, 0, 1, 0, 0,
+                1, 1, 0, 1, 1, 1,
 
-            1,1,1, 0,1,1,
-            1,1,1, 1,0,1,
-            1,1,1, 1,1,0,
+                1, 1, 1, 0, 1, 1,
+                1, 1, 1, 1, 0, 1,
+                1, 1, 1, 1, 1, 0,
 
         }});
-        cube_data.tri={
-            0,1,
-            2,3,
-            4,5,
-            6,7,
-            8,9,
-            10,11,
-            12,13,
-            14,15,
-            16,17,
-            18,19,
-            20,21,
-            22,23,
-            24,25,
-            26,27,
-            28,29,
-            30,31,
-            32,33,
-            34,35,
-            36,37,
-            38,39,
-            40,41
+        cube_data.tri = {
+                0, 1,
+                2, 3,
+                4, 5,
+                6, 7,
+                8, 9,
+                10, 11,
+                12, 13,
+                14, 15,
+                16, 17,
+                18, 19,
+                20, 21,
+                22, 23,
+                24, 25,
+                26, 27,
+                28, 29,
+                30, 31,
+                32, 33,
+                34, 35,
+                36, 37,
+                38, 39,
+                40, 41
         };
-        basic_cube=std::make_shared<gl::mesh>(&cube_data);
+        basic_cube = std::make_shared<gl::mesh>(&cube_data);
     }
 
     void game::loop() {
@@ -120,10 +124,10 @@ namespace client {
         basic_shader->bind();
         basic_shader->uniform4x4("perspective", p);
         basic_shader->uniform4x4("view", v);
-        for(int x=0;x<WORLD_SIZE;x++){
-            for(int y=0;y<16;y++){
-                for(int z=0;z<WORLD_SIZE;z++){
-                    basic_shader->uniform4x4("mode",glm::translate(glm::mat4(1),{x*16,y*16,z*16}));
+        for (int x = 0; x < WORLD_SIZE; x++) {
+            for (int y = 0; y < 16; y++) {
+                for (int z = 0; z < WORLD_SIZE; z++) {
+                    basic_shader->uniform4x4("mode", glm::translate(glm::mat4(1), {x * 16, y * 16, z * 16}));
                     basic_cube->render_lines();
                 }
             }
@@ -138,53 +142,80 @@ namespace client {
 
     }
 
-    void game::download_world(std::string host,std::string port) {
+    void game::download_world(std::string host, std::string port) {
         try {
             printf("START WORLD DOWNLOAD\n");
             boost::asio::io_context io_context;
 
             boost::asio::ip::tcp::resolver resolver(io_context);
-            boost::asio::ip::tcp::resolver::query query(host,port);
-            boost::asio::ip::tcp::resolver::results_type endpoints=resolver.resolve(query);
+            boost::asio::ip::tcp::resolver::query query(host, port);
+            boost::asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(query);
 
             boost::asio::ip::tcp::socket socket(io_context);
-            boost::asio::connect(socket,endpoints);
+            boost::asio::connect(socket, endpoints);
 
             boost::system::error_code err;
 
             printf("START READ\n");
-            for(int x=0;x<WORLD_SIZE;x++) {
+            for (int x = 0; x < WORLD_SIZE; x++) {
                 for (int y = 0; y < 16; y++) {
                     for (int z = 0; z < WORLD_SIZE; z++) {
-                        boost::array<long, 4096>arr{};
+                        boost::array<long, 4096> arr{};
                         size_t len = boost::asio::read(socket, boost::asio::buffer(arr));
 //                        printf("%zu %zu\n",len,sizeof(long),sizeof(long)*4096);
 //                        boost::asio::read(socket,arr);
-                        if(err==boost::asio::error::eof){
-                            printf("UNEXPECTED EOF %i %i %i\n",x,y,z);
+                        if (err == boost::asio::error::eof) {
+                            printf("UNEXPECTED EOF %i %i %i\n", x, y, z);
                             std::raise(11);
-                        }else if(err){
+                        } else if (err) {
                             throw boost::system::system_error(err);
                         }
-                        world->map[x][z]->read(y,arr);
-                        printf("RECIEVED CHUNK %i %i %i\n",x,y,z);
+                        world->map[x][z]->read(y, arr);
+                        printf("RECIEVED CHUNK %i %i %i\n", x, y, z);
                     }
                 }
             }
             printf("END READ\n");
 
+            boost::thread* m_thread;
+            m_thread = new boost::thread([&](){
+                boost::array<long, 1> arr{};
+                size_t len = boost::asio::read(socket, boost::asio::buffer(arr));
+
+                __u_long length_of_nbt = arr[0];
+
+                std::cout << "Length: " << length_of_nbt << std::endl;
+
+                boost::asio::streambuf read_buffer;
+
+                len = boost::asio::read(socket, read_buffer,
+                                                      boost::asio::transfer_exactly(length_of_nbt));
+
+                boost::asio::streambuf::const_buffers_type bufs = read_buffer.data();
+                std::string str(boost::asio::buffers_begin(bufs),
+                                boost::asio::buffers_begin(bufs) + length_of_nbt);
+
+                std::istringstream stream(str);
+
+                std::shared_ptr<nbt::nbt> nbt_obj = nbt::read_nbt(stream);
+
+                std::cout << nbt_obj->to_str(" ") << std::endl;
+
+//                delete[] despacito;
+            });
+
 
             printf("START WORLD RENDER\n");
             for (int x = 0; x < WORLD_SIZE; x++) {
                 for (int z = 0; z < WORLD_SIZE; z++) {
-                    rendered_world[x][z]=std::make_shared<rendered_chunk>(glm::ivec2{x,z});
-                    rendered_world[x][z]->take_chunk(world,world->map[x][z]);
+                    rendered_world[x][z] = std::make_shared<rendered_chunk>(glm::ivec2{x, z});
+                    rendered_world[x][z]->take_chunk(world, world->map[x][z]);
                     rendered_world[x][z]->render_chunk();
                 }
             }
             printf("END WORLD RENDER\n");
-        }catch(std::exception&e){
-            std::cerr<<"client ex caught: "<<e.what()<<"\n";
+        } catch (std::exception& e) {
+            std::cerr << "client ex caught: " << e.what() << "\n";
             std::raise(11);
         }
     }
--- src/server.cpp
+++ src/server.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <nbt/__nbt_float.h>
+#include <nbt/nbt.h>
 #include "world/world.h"
 #include "both.h"
 
@@ -16,8 +18,9 @@ namespace networking {
             world.generate_world();
 
             boost::asio::io_context io_context;
-            boost::asio::ip::tcp::acceptor acceptor(io_context,boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(),port));
-            while(sqrt(5)>0){
+            boost::asio::ip::tcp::acceptor acceptor(io_context,
+                                                    boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port));
+            while (sqrt(5) > 0) {
                 boost::asio::ip::tcp::socket socket(io_context);
                 acceptor.accept(socket);
 
@@ -25,19 +28,46 @@ namespace networking {
 //                boost::array<long,4096> arrs[16*WORLD_SIZE*WORLD_SIZE];
                 printf("SEND WORLD\n");
 //                int i=0;
-                for(int x=0;x<WORLD_SIZE;x++){
-                    for(int y=0;y<16;y++) {
+                for (int x = 0; x < WORLD_SIZE; x++) {
+                    for (int y = 0; y < 16; y++) {
                         for (int z = 0; z < WORLD_SIZE; z++) {
-                            boost::array<long, 4096>arr{};
-                            world.map[x][z]->serialize(y,arr);
+                            boost::array<long, 4096> arr{};
+                            world.map[x][z]->serialize(y, arr);
 //                            i++;
+                            std::cout << "Writing " << x << ", " << y << ", " << z << std::endl;
                             size_t len = boost::asio::write(socket, boost::asio::buffer(arr), err);
+                            std::cout << "Wrote " << x << ", " << y << ", " << z << std::endl;
                         }
                     }
                 }
+
+                std::shared_ptr<nbt::nbt> test = nbt::make_compound({
+                                                                            {"a", nbt::make_float(1.2f)},
+                                                                            {"b", nbt::make_string("asdf")},
+                                                                            {"c", nbt::make_int(47)}
+                                                                    });
+                std::stringstream a;
+                test->write(a);
+
+//                for(int i = 0; i < a.str().length(); i++) {
+//                    printf("%x\n", a.str()[i]);
+//                }
+
+                std::cout << "Writing value " << test->to_str(" ") << std::endl;
+
+                boost::array<__u_long, 1> sz{
+                        a.str().length()
+                };
+
+                std::cout << "Length: " << sz[0] << std::endl;
+                std::cout << "Data: " << a.str() << std::endl;
+//                sz[0] = a.str().length();
+                boost::asio::write(socket, boost::asio::buffer(sz));
+                boost::asio::write(socket, boost::asio::buffer(a.str()));
+                std::cout << "wrote" << std::endl;
 //                boost::asio::write(socket,boost::asio::buffer(arrs),err);
             }
-        }catch (std::exception& e) {
+        } catch (std::exception& e) {
             std::cerr << "server ex caught: " << e.what() << "\n";
         }
 
