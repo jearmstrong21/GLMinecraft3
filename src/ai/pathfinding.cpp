57682a796cf9ef0f2a759a3ed82e4edc7b7a06ac
Jack Armstrong <mackycheese21@gmail.com>
2020-01-13 20:25:29 -0800

    Pathfinding;

---
+++ src/ai/pathfinding.cpp
@@ -0,0 +1,107 @@
+//
+// Created by Jack Armstrong on 1/13/20.
+//
+
+#include "pathfinding.h"
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <random>
+
+namespace ai::path {
+
+    path_options default_opt(){
+        path_options opt;
+        opt.ok_distance=0.5F;
+        opt.max_search_nodes=500;
+        opt.cardinal_directions={{-1,0,0},{1,0,0},{0,0,-1},{0,0,1}};
+        opt.can_fall=true;
+        opt.can_jump=true;
+        return opt;
+    }
+
+    glm::vec3 pathfind_astar(allowed_at_pos_func allowed_at,glm::vec3 start,glm::vec3 end,const path_options& opt){
+        std::vector<glm::vec3>edgeNodes;
+        std::vector<glm::vec3>processedNodes;
+        std::map<glm::vec3,float>pathLengths;
+        std::map<glm::vec3,glm::vec3>pathOrigins;
+        //TODO: should edgeNodes and processedNodes be set/set or priority q/set
+
+        auto fround=[](float f){
+            return (float)((int)(f*2))/(float)2.0F;
+        };
+
+        auto heuristic=[](glm::vec3 a,glm::vec3 b){
+            return abs(a.x-b.x)+abs(a.y-b.y)+abs(a.z-b.z);
+        };
+
+        auto neighbors=[&allowed_at,opt](glm::vec3 v){
+            std::vector<glm::vec3>l;
+            for(glm::vec3 c:opt.cardinal_directions){
+                if(allowed_at(v+c*opt.node_dist)){
+                    l.push_back(v+c*opt.node_dist);
+                }
+            }
+            if(allowed_at(v+glm::vec3{0,1,0})){
+                l.push_back(v+glm::vec3{0,1,0});
+            }
+            if(allowed_at(v+glm::vec3{0,-1,0})){
+                l.push_back(v+glm::vec3{0,-1,0});
+            }
+            return l;
+        };
+
+        start.x=fround(start.x);
+        start.y=fround(start.y);
+        start.z=fround(start.z);
+
+        end.x=fround(end.x);
+        end.y=fround(end.y);
+        end.z=fround(end.z);
+
+        edgeNodes.push_back(start);
+        pathLengths[start]=0;
+        pathOrigins[start]=start;
+        int i=0;
+        while(!edgeNodes.empty()){
+            if(i>opt.max_search_nodes){
+                return {0,0,0};
+            }
+            i++;
+            int bestIndex=0;
+            for(int n=1;n<edgeNodes.size();n++){
+                if(heuristic(end,edgeNodes[n])<heuristic(end,edgeNodes[bestIndex]))bestIndex=n;
+            }
+            glm::vec3 v=edgeNodes[0];
+            edgeNodes.erase(edgeNodes.begin());
+            if(std::find(processedNodes.begin(),processedNodes.end(),v)!=processedNodes.end())continue;
+            processedNodes.push_back(v);
+            if(heuristic(v,end)<=opt.ok_distance){
+                break;
+            }
+            std::vector<glm::vec3>children=neighbors(v);
+            for(glm::vec3 child:children){
+                float tentative=pathLengths[v]+1;
+                if(pathLengths.find(child)==pathLengths.end()||pathLengths[child]>tentative){
+                    pathLengths[child]=tentative;
+                    pathOrigins[child]=v;
+                    if(std::find(edgeNodes.begin(),edgeNodes.end(),child)==edgeNodes.end())edgeNodes.push_back(child);
+                }
+            }
+        }
+        std::vector<glm::vec3>path;
+        glm::vec3 v=end;
+        while(true){
+            if(pathOrigins.find(v)!=pathOrigins.end()){
+                glm::vec3 orig=v;
+                v=pathOrigins[v];
+                if(orig==v)break;
+            }else break;
+            path.push_back(v);
+        }
+        std::reverse(path.begin(),path.end());
+        if(path.size()<2)return {0,0,0};
+        return glm::normalize(path[1]-path[0]);
+    }
+
+}
---
+++ src/ai/pathfinding.h
@@ -0,0 +1,32 @@
+//
+// Created by Jack Armstrong on 1/13/20.
+//
+
+#ifndef GLMINECRAFT3_PATHFINDING_H
+#define GLMINECRAFT3_PATHFINDING_H
+
+#include <functional>
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace ai::path {
+
+    struct path_options{
+        int max_search_nodes;//raw max num of nodes to look at, approx search range = cbrt(max_search_nodes)
+        float ok_distance;//terminate if within ok_distance of end; setting to zero almost always will not return valid result
+        std::vector<glm::vec3> cardinal_directions;//left/right/front/back
+        float node_dist;//distance between two nodes
+        bool can_jump;//up (gravity affected)
+        bool can_fall;//down (gravity)
+    };
+
+    path_options default_opt();
+
+    typedef std::function<bool(glm::vec3)> allowed_at_pos_func;
+    typedef std::function<glm::vec3(allowed_at_pos_func,glm::vec3,glm::vec3,path_options)> pathfinder_func;//allowed_at,start,end,opt
+
+    glm::vec3 pathfind_astar(allowed_at_pos_func allowed_at,glm::vec3 start,glm::vec3 end,const path_options& opt);
+
+}
+
+#endif //GLMINECRAFT3_PATHFINDING_H
