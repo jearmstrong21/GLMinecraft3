b1e5a1eb6da46ce2f2076b49df18520e056c4712
Jack Armstrong <mackycheese21@gmail.com>
2020-01-17 21:48:16 -0800

    format;

--- src/item/item.h
+++ src/item/item.h
@@ -20,22 +20,25 @@ namespace item {
         }
     };
 
-    typedef std::shared_ptr<item_stack>item_stack_ptr;
+    typedef std::shared_ptr<item_stack> item_stack_ptr;
 
     struct item {
-        virtual void attack(item_stack_ptr stack)=0;
-        virtual void use(item_stack_ptr stack)=0;
+        virtual void attack(item_stack_ptr stack) = 0;
+
+        virtual void use(item_stack_ptr stack) = 0;
         //TODO: item should implement default attack / use
 
         //TODO: save/load name, count, enchants
         //TODO: save/load should call save_additional_information / load_additional_information
-        void save(item_stack_ptr stack, const nbt::nbt_compound_ptr&tag);
-        virtual void save_additional_information(item_stack_ptr stack,const nbt::nbt_compound_ptr&tag)=0;
+        void save(item_stack_ptr stack, const nbt::nbt_compound_ptr &tag);
 
-        void load(item_stack_ptr stack,const nbt::nbt_compound_ptr& tag);
-        virtual void load_additional_information(item_stack_ptr stack,const nbt::nbt_compound_ptr&tag)=0;
+        virtual void save_additional_information(item_stack_ptr stack, const nbt::nbt_compound_ptr &tag) = 0;
 
-        virtual item_stack make(int count)=0;
+        void load(item_stack_ptr stack, const nbt::nbt_compound_ptr &tag);
+
+        virtual void load_additional_information(item_stack_ptr stack, const nbt::nbt_compound_ptr &tag) = 0;
+
+        virtual item_stack make(int count) = 0;
     };
 }
 
